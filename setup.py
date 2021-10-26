fc9e1f4c0854a4f43c9d693bbf682d5d03ee31ea
Jagar <jagaryousef@my.uopeople.edu>
2021-10-27 02:07:30 +0300

    fix requirments.txt bug

--- setup.py
+++ setup.py
@@ -1,16 +1,24 @@
 from setuptools import setup, find_packages
 import codecs
 import os
+import pathlib
+import pkg_resources
 
 here = os.path.abspath(os.path.dirname(__file__))
 
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.0.7'
+VERSION = '0.0.8'
 DESCRIPTION = 'Convert Kurdish text to IPA phonetics'
 LONG_DESCRIPTION = 'A package that convert Kurdish texts to phonetics, and other related tools'
 
+with pathlib.Path('requirements.txt').open() as requirements_txt:
+    install_requires = [
+        str(requirement)
+        for requirement
+        in pkg_resources.parse_requirements(requirements_txt)
+    ]
 # Setting up
 setup(
     name="kipa",
@@ -23,6 +31,7 @@ setup(
     packages=find_packages(),
     setup_requires=['wheel'],
     url='https://github.com/kurd-cc/kipa',
+    install_requires=install_requires,
     keywords=['kurdish', 'language', 'language-processing', 'kurmanji', 'ipa', 'phonetics'],
     classifiers=[
         "Development Status :: 1 - Planning",
